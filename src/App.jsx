32133917c523e0b7bb435389c177d4b335895ef1
johnlombardi389 <lombardij38@gmail.com>
2024-04-02 17:13:44 -0400

    Styled a bouncng dot animation for loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   useEffect(() => {
     const introTimeout = setTimeout(() => {
       setShowIntro(false);
-    }, 3000);
+    }, 3500);
 
     return () => clearTimeout(introTimeout);
   }, []);
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,4 +1,16 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const bounce = keyframes`
+  0% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-10px);
+  }
+  100% {
+    transform: translateY(0);
+  }
+`;
 
 const Intro = () => {
   return (
@@ -6,6 +18,11 @@ const Intro = () => {
       <Title>John Lombardi</Title>
       <Subtitle>Email Developer</Subtitle>
       <Description>I make cool emails for you.</Description>
+      <LoadingIndicator>
+        <DelayedDot />
+        <DelayedDot />
+        <DelayedDot />
+      </LoadingIndicator>
     </IntroContainer>
   );
 };
@@ -41,4 +58,31 @@ const Description = styled.p`
   color: #999;
   font-family: "Open Sans", sans-serif;
   font-weight: 300;
+`;
+
+const Dot = styled.div`
+  width: 10px;
+  height: 10px;
+  background-color: #fff;
+  border-radius: 50%;
+  margin: 0 5px;
+  animation: ${bounce} 1s ease-in-out infinite alternate;
+`;
+
+const LoadingIndicator = styled.div`
+  margin-top: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+const DelayedDot = styled(Dot)`
+  &:nth-child(1) {
+    animation-delay: 0.1s;
+  }
+  &:nth-child(2) {
+    animation-delay: 0.2s;
+  }
+  &:nth-child(3) {
+    animation-delay: 0.3s;
+  }
 `;
