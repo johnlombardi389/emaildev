99ed3763c23221ee0481d1caacc019cc4bb159d9
johnlombardi389 <lombardij38@gmail.com>
2024-04-02 17:06:21 -0400

    Styled intro page font and centered all text

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,11 +1,44 @@
+import styled from "styled-components";
+
 const Intro = () => {
   return (
-    <div>
-      <h1>John Lombardi</h1>
-      <p>Email Developer</p>
-      <p>I make cool emails for you.</p>
-    </div>
+    <IntroContainer>
+      <Title>John Lombardi</Title>
+      <Subtitle>Email Developer</Subtitle>
+      <Description>I make cool emails for you.</Description>
+    </IntroContainer>
   );
 };
 
 export default Intro;
+
+const IntroContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 3rem;
+  color: #333;
+  margin-bottom: 10px;
+  font-family: "Signika", sans-serif;
+  font-weight: 600;
+`;
+
+const Subtitle = styled.p`
+  font-size: 1.5rem;
+  color: #666;
+  margin-bottom: 5px;
+  font-family: "Open Sans", sans-serif;
+  font-weight: 400;
+`;
+
+const Description = styled.p`
+  font-size: 1rem;
+  color: #999;
+  font-family: "Open Sans", sans-serif;
+  font-weight: 300;
+`;
