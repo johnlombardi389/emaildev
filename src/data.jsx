c10d6b28619be82a0d221462787b250091480b2a
johnlombardi389 <lombardij38@gmail.com>
2024-04-03 13:57:37 -0400

    Updated project images

--- src/data.jsx
+++ src/data.jsx
@@ -1,17 +1,21 @@
 // Images
 import ProjectOne from "./assets/project1.jpeg";
+import Nytimescooking from "./assets/nytimescooking.png";
+import Kitchenaid from "./assets/kitchenaid.png";
+import Capcom from "./assets/capcom.png";
+import Uber from "./assets/uber.png";
 
 const projectData = () => [
   {
     id: 1,
-    tag: "Newsletter",
-    title: "NYT Cooking Newsletter",
+    tag: "Promotional",
+    title: "Uber Promotional Email",
     tools: ["HTML", "&#x2022;", "CSS", "&#x2022;", "MJML"],
-    demo: "https://johnlombardi389.github.io/nytcookingemail/",
-    github: "https://github.com/johnlombardi389/nytcookingemail",
-    image: ProjectOne,
+    demo: "https://johnlombardi389.github.io/uberemail/",
+    github: "https://github.com/johnlombardi389/uberemail",
+    image: Uber,
     description:
-      "Hello this is the first project where I created an html email for cooking.",
+      "Hello this is the fourth project where I made a promotional email for Uber.",
   },
   {
     id: 2,
@@ -20,7 +24,7 @@ const projectData = () => [
     tools: ["HTML", "&#x2022;", "CSS"],
     demo: "https://johnlombardi389.github.io/kitchenaidemail/",
     github: "https://github.com/johnlombardi389/kitchenaidemail",
-    image: ProjectOne,
+    image: Kitchenaid,
     description:
       "Hello this is the second project where I made a transaction email for Kitchen Aid order.",
   },
@@ -31,20 +35,20 @@ const projectData = () => [
     tools: ["HTML", "&#x2022;", "CSS", "&#x2022;", "MJML"],
     demo: "https://johnlombardi389.github.io/capcomemail/",
     github: "https://github.com/johnlombardi389/capcomemail",
-    image: ProjectOne,
+    image: Capcom,
     description:
       "Hello this is the third project where I made a promotional email for Capcom.",
   },
   {
     id: 4,
-    tag: "Promotional",
-    title: "Uber Promotional Email",
+    tag: "Newsletter",
+    title: "NYT Cooking Newsletter",
     tools: ["HTML", "&#x2022;", "CSS", "&#x2022;", "MJML"],
-    demo: "https://johnlombardi389.github.io/uberemail/",
-    github: "https://github.com/johnlombardi389/uberemail",
-    image: ProjectOne,
+    demo: "https://johnlombardi389.github.io/nytcookingemail/",
+    github: "https://github.com/johnlombardi389/nytcookingemail",
+    image: Nytimescooking,
     description:
-      "Hello this is the fourth project where I made a promotional email for Uber.",
+      "Hello this is the first project where I created an html email for cooking.",
   },
 ];
 
